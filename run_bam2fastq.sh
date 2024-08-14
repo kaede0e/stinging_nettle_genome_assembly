4b5900d70677e151e1a86570a04868871bd884b7
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-08-13 21:43:26 -0700

    Create run_bam2fastq.sh

---
+++ run_bam2fastq.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=$accountname
+#SBATCH --time=1-0
+#SBATCH --mem=30Gb
+#SBATCH --output=/home/kaedeh/scratch/Nettle/log_file/bam2fastq_nettle_male.12Mar2024.out
+#SBATCH --error=/home/kaedeh/scratch/Nettle/log_file/bam2fastq_nettle_male.12Mar2024.err
+
+SMRT_ROOT=/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/pacbio/smrtlink
+export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/pacbio/smrtlink/smrtcmds/bin/
+
+bam2fastq -o Pacbio_hifi/Nettle_male_Pacbio_hifi Pacbio_hifi/Urtica_male_m84185_240301_023219_s3.hifi_reads.bam
