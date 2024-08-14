153b78880a0d326c688d7f678671e647fd4362b1
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-08-13 22:07:49 -0700

    Create run_PLOTS_Per_Chr_allchr.R.sh

---
+++ 1_genome_assembly_with_PBHiFi_HiC/run_PLOTS_Per_Chr_allchr.R.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=5000M
+
+module load StdEnv/2023 r/4.4.0
+
+for j in $( ls *tab_fp.txt)
+do
+   for i in $(cut -f 7 $j | sort | uniq)
+   do
+   Rscript PLOTS_Per_Chr_allchr.R $j $i
+   done
+done
