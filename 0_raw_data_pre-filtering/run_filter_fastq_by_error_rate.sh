fce6ba66a993b10ee0ea74b4e7375313c20caf08
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-11-21 10:50:28 -0800

    Update run_filter_fastq_by_error_rate.sh

--- 0_raw_data_pre-filtering/run_filter_fastq_by_error_rate.sh
+++ 0_raw_data_pre-filtering/run_filter_fastq_by_error_rate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=1-0
 #SBATCH --mem=20G
-#SBATCH --account=def-gowens
+#SBATCH --account=
 
 module load python/3.10
-source /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/python_env/bin/activate
+source /home/~/bin/python_env/bin/activate
 
 fastq-filter -e 0.01 -o Nettle_female_Pacbio_hifi_Q20_filtered.fastq Nettle_female_Pacbio_hifi.fastq
