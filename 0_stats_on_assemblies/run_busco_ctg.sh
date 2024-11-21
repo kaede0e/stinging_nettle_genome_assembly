449019678c64375ee70f4f41c922961e3dd14ba7
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-11-21 10:52:18 -0800

    Update run_busco_ctg.sh

--- 0_stats_on_assemblies/run_busco_ctg.sh
+++ 0_stats_on_assemblies/run_busco_ctg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --account=def-gowens
+#SBATCH --account=
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=6000M
@@ -11,13 +11,13 @@
 # you're suggested to work in a new screen so that all softwares are properly loaded.
 module --force purge
 module load StdEnv/2020 gcc python augustus hmmer blast+ metaeuk prodigal r
-source /home/kaedeh/projects/def-gowens/kaedeh/Nettle/scripts/busco_env/bin/activate
+source /home/~/scripts/busco_env/bin/activate
 
 # Run BUSCO job:
 busco --offline \
 --in /home/kaedeh/scratch/Nettle/assembly/hifiasm_asm_4/Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap1.p_ctg.fa \
 --out Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap1._BUSCO_output \
---lineage_dataset /home/kaedeh/projects/def-gowens/kaedeh/Nettle/scripts/busco_downloads/lineages/eudicots_odb10 \
+--lineage_dataset /home/~/busco_downloads/lineages/eudicots_odb10 \
 --mode genome \
 --cpu ${SLURM_CPUS_PER_TASK-1}
 
