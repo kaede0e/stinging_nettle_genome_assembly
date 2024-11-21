13c0413bf48111cd4c0d3271faa6fc40673c7072
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-11-21 11:02:17 -0800

    Update run_genespace_with_orthofinder_pre-parsed.R.sh

--- 3_comparative_genomics/run_genespace_with_orthofinder_pre-parsed.R.sh
+++ 3_comparative_genomics/run_genespace_with_orthofinder_pre-parsed.R.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=1-00:00:00
-#SBATCH --account=rrg-rieseber-ac
+#SBATCH --account=
 #SBATCH --ntasks=1
 #SBATCH --mem=90G
 #SBATCH --cpus-per-task=32
 #SBATCH --job-name=GENESPACE_comparative_genomics_cont
-#SBATCH --output=/home/kaedeh/scratch/Nettle/log_file/Nettle.genespaceR_cont.21Oct2024.out
-#SBATCH --error=/home/kaedeh/scratch/Nettle/log_file/Nettle.genespaceR_cont.21Oct2024.err
+#SBATCH --output=
+#SBATCH --error=
 
 
 # ---------------------------------------------------------------------
@@ -19,8 +19,8 @@ echo ""
 ##### Make sure required modules are installed for Orthofiner, MCScan, to run GENESPACE #####
 module load StdEnv/2020 gcc/9.3.0 python/3.8 java/13.0.2 diamond/2.0.15 r/4.3.1
 source /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/smcpp_python_env/bin/activate
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/OrthoFinder_source
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/MCScanX
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/MCScanX/downstream_analyses
+export PATH=$PATH:/home/~/bin/OrthoFinder_source
+export PATH=$PATH:/home/~/bin/MCScanX
+export PATH=$PATH:/home/~/bin/MCScanX/downstream_analyses
 
 Rscript genespace_with_orthofinder_pre-parsed.R
