601c83ded93f538c8732be5a0a49641d5ec0b866
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-11-21 10:59:47 -0800

    Update run_ragtag_correct_scaffold.sh

--- 1_genome_assembly_with_PBHiFi_HiC/run_ragtag_correct_scaffold.sh
+++ 1_genome_assembly_with_PBHiFi_HiC/run_ragtag_correct_scaffold.sh
@@ -2,11 +2,11 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=250G
 #SBATCH --ntasks=1
-#SBATCH --account=rrg-rieseber-ac
+#SBATCH --account=
 #SBATCH --cpus-per-task=4
 #SBATCH --job-name=Nettle_assembly_ragtag_with_DToL_refgenome
-#SBATCH --output=/home/kaedeh/scratch/Nettle/log_file/Nettle_female.ragtag.correct.scaffold.hap1.20Sep2024.out
-#SBATCH --error=/home/kaedeh/scratch/Nettle/log_file/Nettle_female.ragtag.correct.scaffold.hap1.20Sep2024.err
+#SBATCH --output=
+#SBATCH --error=
 
 #####################################
 ### Execution of programs ###########
@@ -22,11 +22,11 @@ echo ""
 ##### Ragtag - reference genome based scaffolding #####
 
 module load StdEnv/2020 minimap2/2.24
-source /home/kaedeh/projects/def-gowens/kaedeh/Nettle/scripts/busco_env/bin/activate
+source /home/~/scripts/busco_env/bin/activate
 
 refgenome=GCA_964188135.1_drUrtDioi1.1_genomic.chrname_modified.chr.fa
-qrygenome_H1=~/projects/def-gowens/kaedeh/Nettle/output/assembly/Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap1.p_ctg.fa
-qrygenome_H2=~/projects/def-gowens/kaedeh/Nettle/output/assembly/Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap2.p_ctg.fa
+qrygenome_H1=~/Nettle/output/assembly/Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap1.p_ctg.fa
+qrygenome_H2=~/Nettle/output/assembly/Nettle_female_hifi_hifiasm_0.19.8_Q20reads.asm.hic.hap2.p_ctg.fa
 
 ragtag.py correct \
 $refgenome \
