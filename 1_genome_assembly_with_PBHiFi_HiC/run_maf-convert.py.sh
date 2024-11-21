5be77500c10de936b69123a527d0e8d4e0cacce7
kaede0e <91504464+kaede0e@users.noreply.github.com>
2024-11-21 10:58:55 -0800

    Update run_maf-convert.py.sh

--- 1_genome_assembly_with_PBHiFi_HiC/run_maf-convert.py.sh
+++ 1_genome_assembly_with_PBHiFi_HiC/run_maf-convert.py.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=def-mtodesco
+#SBATCH --account=
 #SBATCH --time=24:00:00
 #SBATCH --mem=50G
 
 module load StdEnv/2020 python/2.7.18
 
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/anchorwave/
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/anchorwave/scripts
+export PATH=$PATH:/home/~/bin/anchorwave/
+export PATH=$PATH:/home/~/bin/anchorwave/scripts
 
 python2 maf-convert.py tab anchorwave_proali_Nettle_female_Hap1_vs_Hap2.maf > anchorwave_proali_Nettle_female_Hap1_vs_Hap2.tab
