515d48e9c82cce9c14973d9b3fb08122ec933afb
kaede0e <91504464+kaede0e@users.noreply.github.com>
2025-08-01 11:50:45 -0700

    Update run_bbmap.sh

--- 0_stats_on_assemblies/run_bbmap.sh
+++ 0_stats_on_assemblies/run_bbmap.sh
@@ -7,7 +7,7 @@ salloc --account=rrg-rieseber-ac --time=3:00:00 --ntasks=1 --cpus-per-task=4 --m
 module load StdEnv/2023 bbmap/39.06 #we'll use the software called BBMap https://archive.jgi.doe.gov/data-and-tools/software-tools/bbtools/bb-tools-user-guide/bbmap-guide/
 your_genome=Sclerotinia_minor_hap
 
-stats.sh -Xmx20g $your_genome.fa format=2
+stats.sh -Xmx20g ${your_genome}.fa format=2
 
 #results will be printed on the screen, or you can save it as a text file. 
-stats.sh -Xmx20g $your_genome.fa format=2 2> bbmap_output_${your_genome}.txt
+stats.sh -Xmx20g ${your_genome}.fa format=2 2> bbmap_output_${your_genome}.txt
