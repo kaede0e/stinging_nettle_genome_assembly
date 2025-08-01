0a209096f6fd9517e1317429771f334fe4cec73f
kaede0e <91504464+kaede0e@users.noreply.github.com>
2025-08-01 12:07:11 -0700

    Update run_busco_ctg.sh

--- 0_stats_on_assemblies/run_busco_ctg.sh
+++ 0_stats_on_assemblies/run_busco_ctg.sh
@@ -1,3 +1,21 @@
+# BUSCO with contig file (fasta)
+# BUSCO requires you to work in a Python Wheel environment.
+# So first, you create a Python environment in your workspace.
+# you're suggested to work in a new screen so that all softwares are properly loaded.
+module load StdEnv/2020 gcc/9.3.0 python/3.10 augustus/3.5.0 hmmer/3.3.2 blast+/2.13.0 metaeuk/6 prodigal/2.6.3 r/4.3.1 bbmap/38.86
+virtualenv busco_env_v2
+source busco_env/bin/activate
+pip install --no-index biopython==1.81 pandas==2.1.0 busco==5.5.0 #as of July 2025 (thanks Vedin for figuring out!)
+
+# If working properly, you will be directed to a python environment like this:
+## (busco_env) [kaedeh@cdr767 strawberry_genome]$
+
+# Download the appropriate lineage dataset by: 
+busco --download eudicots_odb10
+
+
+# You're good to proceed.
+
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --account=
@@ -5,13 +23,12 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=6000M
 
-# BUSCO with contig file (fasta)
-# BUSCO requires you to work in a Python Wheel environment.
-# So first, you create a Python environment in your workspace.
-# you're suggested to work in a new screen so that all softwares are properly loaded.
-module --force purge
-module load StdEnv/2020 gcc python augustus hmmer blast+ metaeuk prodigal r
-source /home/~/scripts/busco_env/bin/activate
+# Run BUSCO job: (fungal genome example)
+busco --offline \
+--in with_newhic/Sclerotinia_minor_hifiasm_1.asm.hic.hap1.p_ctg.fa \
+--out Sclerotinia_minor_hifiasm1_newhic_H1.BUSCO_output \
+--lineage_dataset /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/busco_env/reference_data/busco_downloads/lineages/helotiales_odb10 \
+--mode genome --cpu 4
 
 # Run BUSCO job:
 busco --offline \
