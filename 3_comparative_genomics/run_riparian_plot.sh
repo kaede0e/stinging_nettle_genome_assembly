661ae4a8e565a43f6ac57817730ea3c65e43f6bd
kaede0e <91504464+kaede0e@users.noreply.github.com>
2026-02-05 13:05:15 -0800

    Update run_riparian_plot.R

--- 3_comparative_genomics/run_riparian_plot.sh
+++ 3_comparative_genomics/run_riparian_plot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/r
 # Riparian plotting! 
 # get an interactive job, load all modules and open R for riparian plotting: 
 genomeRepo <- "/home/kaedeh/projects/def-gowens/kaedeh/Nettle/raw_data/reference_data/genomeRepo"
