5ce5777f7d213ad58e1dc05ef32b915df500b820
kaede0e <91504464+kaede0e@users.noreply.github.com>
2026-02-19 11:49:24 -0800

    Update run_aln_plots_R.sh

--- 1_genome_assembly_with_PBHiFi_HiC/run_aln_plots_R.sh
+++ 1_genome_assembly_with_PBHiFi_HiC/run_aln_plots_R.sh
@@ -3,12 +3,48 @@
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=15
 #SBATCH --account=
+#SBATCH --output=/home/kaedeh/scratch/Nettle/log_file/Nettle_female_hap1_hap2_minimap2_aln.23May2024.out
+#SBATCH --error=/home/kaedeh/scratch/Nettle/log_file/Nettle_female_hap1_hap2_minimap2_aln.23May2024.err
+
+### quality check the Juicer+3ddna pipeline output by aligning two haplotypes ###
+
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+echo "SLURM_JOBID: " $SLURM_JOBID
+# ---------------------------------------------------------------------
+echo ""
+
+#####################################
+### Execution of programs ###########
+#####################################
+
+module load StdEnv/2023 minimap2/2.28
+
+#Steo1 run minimap2 alignment
+minimap2 -cx asm5 Round_2_hap1.reviewed.chr_assembled.fasta Round_2_hap2.reviewed.chr_assembled.fasta > Nettle_female_hap1_hap2_aln.paf
+
+#Step2 convert paf to table to plot and select alignments with MQ > 30
+for i in $(ls *paf)
+do
+        name=$(echo $i | cut -d "." -f 1)
+        awk '$12 >= 30 {print $8, $9, $3, $4, "30", $1, $6, $5 }' $i > "$name""MQ30_tab.txt"
+done
+
+
+#------------------------------------------# 
+#!/bin/bash
+#SBATCH --time=5:00:00
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=15
+#SBATCH --account=
 #SBATCH --output=
 #SBATCH --error=
 
 module load r/4.4
 
 ### conver the aln.paf from minimap2 into a visual plot in ggplot2 from R script ###
+# Make sure you have the aln_plots.R script from Eric copied to your directory. 
 
 #Step3 R plots
 
